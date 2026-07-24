fac93fbfb1077ca81ee62848ebd0381c6762d772
Siim Liimand <siim.liimand@gmail.com>
2026-07-24 11:27:46 +0300

    1.4: Create tailwind.config.js with content paths and safelist

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/Views/**/*.php',
+    './woocommerce/**/*.php',
+    './header.php',
+    './footer.php',
+    './index.php',
+    './assets/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
